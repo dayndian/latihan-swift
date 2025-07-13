d531f90d376b568984e281740b77d7c9e985e2dc
dyan <penggaimidian@gmail.com>
2025-07-13 21:18:05 +0700

    Create Operator_Perbandingan.swift
    operator perbandingan swift

---
+++ Operator_Perbandingan.swift
@@ -0,0 +1,24 @@
+
+//Sama dengan/equal to (a == b)
+//Tidak sama dengan/not equal to (a != b)
+//Lebih besar dari/greater than (a > b)
+//Kurang dari/less than (a < b)
+//Lebih besar sama dengan/greater than or equal to (a >= b)
+//Kurang dari sama dengan/less than or equal to (a <= b)
+let equalTo = 1 == 1   
+print("\(equalTo) karena 1 sama dengan 1")
+
+let notEqualTo = 2 != 1   
+print("\(notEqualTo) karena 2 tidak sama dengan 1")
+
+let greaterThan = 2 > 1    
+print("\(greaterThan) karena 2 lebih besar dari 1")
+
+let lessThan = 1 < 2    
+print("\(lessThan) karena 1 lebih kecil dari 2")
+
+let greaterThanOrEqualTo = 1 >= 1
+print("\(greaterThanOrEqualTo) karena 1 lebih besar sama dengan 1")
+
+let lessThanOrEqualTo = 1 <= 2
+print("\(lessThanOrEqualTo) karena 1 lebih kecil sama dengan 2")
