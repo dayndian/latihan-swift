9e28b731a5b3c4925886481eaf23958d67b05f93
dyan <penggaimidian@gmail.com>
2025-07-13 18:03:27 +0700

    Create main.swift
    latihan koding swift dengan xcode

---
+++ main.swift
@@ -0,0 +1,22 @@
+var friendlyWelcome = "Hello!"
+friendlyWelcome = "Bonjour!"
+print("The current value of friendlyWelcome is \(friendlyWelcome)")
+
+
+print(friendlyWelcome)
+
+
+print(1.0, 2.0, 3.0, 4.0, 5.0, separator: " ... ")
+
+print(1.0, 2.0, 3.0, 4.0, 5.0)
+
+for n in 1...5 {
+    print(n, terminator: "")
+}
+
+
+/* 
+  Ini contoh blok pertama
+  /* Ini contoh blok kedua */
+  Ini masih di blok pertama 
+  */
