e55dc1dad6f0e678a818dc95e0b78c59c6dd1ee3
dyan <penggaimidian@gmail.com>
2025-07-13 18:29:49 +0700

    Create main3.swift
    Angka desimal, tanpa awalan.
    Angka biner, dengan awalan 0b.
    Angka oktal, dengan awalan 0o.
    Angka heksadesimal, dengan awalan 0x.

---
+++ main3.swift
@@ -0,0 +1,28 @@
+//float-point number
+let apple = 3.5
+let banana: Double = 4.1
+let manggo: Float = 2.4
+
+let age = 42
+
+//Numeric Literals
+let decimalInteger = 18
+print("Nilai decimalInteger adalah \(decimalInteger)")
+
+let binaryInteger = 0b10010      
+print("Nilai binaryInteger adalah \(binaryInteger)")
+
+let octalInteger = 0o22          
+print("Nilai octalInteger adalah \(octalInteger)")
+
+let hexadecimalInteger = 0x12    
+print("Nilai hexadecimalInteger adalah \(hexadecimalInteger)")
+
+let decimalExponen = 1.25e2              
+print("1.25e2 berarti 1.25 x 10 pangkat 2 atau \(decimalExponen).")
+
+
+
+let anotherDecimalExponen = 1.25e-2     
+print("1.25e-2 berarti 1.25 x 10 pangkat -2 atau \(anotherDecimalExponen).")
+
