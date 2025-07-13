55a1eff5857c145b46b32815a4773a1fe7ad128f
dyan <penggaimidian@gmail.com>
2025-07-13 18:55:55 +0700

    Create tuple.swift
    latihan tuple swift

---
+++ tuple.swift
@@ -0,0 +1,3 @@
+let http200Status = (statusCode: 200, description: "OK")
+print("http200Status memiliki kode status \(http200Status.statusCode)")
+print("http200Status memiliki pesan error \"\(http200Status.description)\"")
