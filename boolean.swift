01f8f697e35a80bcf6584e5b35713f6d5757e503
dyan <penggaimidian@gmail.com>
2025-07-13 18:48:34 +0700

    Create boolean.swift
    latihan boolean swift

---
+++ boolean.swift
@@ -0,0 +1,14 @@
+
+let orangesAreOrange = true
+let turnipsAreDelicious = false
+
+if turnipsAreDelicious {
+   print("Mmm, lobaknya lezat!")
+} else {
+   print("Eww, lobak itu mengerikan.")
+}
+
+let i = 1
+if i == 1 {
+  print("Pernyataan bersyarat berhasil di-compile dengan baik.")
+}
