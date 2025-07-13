f7f98a1507b740f8793fdc4d7bf7ce15ca9a76dc
dyan <penggaimidian@gmail.com>
2025-07-13 18:31:34 +0700

    Create main4.swift

---
+++ main4.swift
@@ -0,0 +1,4 @@
+let hexadecimalExponen = 0xFp2          
+print("0xFp2 berarti 15 x 2 pangkat 2 atau \(hexadecimalExponen).")
+let anotherHexadecimalExponen = 0xFp-2
+print("0xFp-2 berarti 15 x 2 pangkat -2 atau \(anotherHexadecimalExponen).")
