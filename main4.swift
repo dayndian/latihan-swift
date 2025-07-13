061f39efdf0b7104f8bbd6b7c515fd78c5335d21
dyan <penggaimidian@gmail.com>
2025-07-13 18:36:34 +0700

    Update main4.swift

--- main4.swift
+++ main4.swift
@@ -2,3 +2,20 @@ let hexadecimalExponen = 0xFp2          
 print("0xFp2 berarti 15 x 2 pangkat 2 atau \(hexadecimalExponen).")
 let anotherHexadecimalExponen = 0xFp-2
 print("0xFp-2 berarti 15 x 2 pangkat -2 atau \(anotherHexadecimalExponen).")
+
+//integer
+
+let twoThousand: UInt16 = 2_000
+let one: UInt8 = 1
+let twoThousandAndOne = twoThousand + UInt16(one)
+print(twoThousandAndOne)
+
+//Integer and Floating-Point Conversion
+
+let three = 3
+let pointOneFourOneFiveNine = 0.14159
+let pi = Double(three) + pointOneFourOneFiveNine
+print("Nilai pi adalah \(pi), dan ini di definisikan menjadi tipe Double.")
+
+
+
